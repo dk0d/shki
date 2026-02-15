1ced329669083ef338b01cfa08afd23faf49db0e
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-02-15 12:30:18 -0500

    fix: error handling in migrate command

--- src/cli/commands/migrate.rs
+++ src/cli/commands/migrate.rs
@@ -1,20 +1,25 @@
 use crate::config::Config;
 
-use crate::{MigrationManager, Result, ShkiError, create_any_pool};
+use crate::{MigrationManager, Result, ShkiError, create_any_pool_opts};
 use colored::Colorize;
 
 use crate::cli::commands::status::display_migrations;
 use sqlx::AnyPool;
 
 pub async fn cmd_migrate(config: &Config, dry_run: bool) -> Result<()> {
+    if let Some(url) = config.database_url.as_ref() {
+        println!("\nURL {}", url.bright_green());
+    }
+
     let db_url = config
         .database_url
         .as_ref()
         .ok_or_else(|| ShkiError::config("DATABASE_URL is required"))?;
 
-    sqlx::any::install_default_drivers();
-
-    let pool: AnyPool = create_any_pool(db_url).await;
+    let pool: AnyPool = create_any_pool_opts()
+        .max_connections(2)
+        .connect(db_url)
+        .await?;
 
     let migration_manager = MigrationManager::new(&config.out, config.dialect)
         .with_table_name(&config.migrations.table)
