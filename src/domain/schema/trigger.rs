a1dcf49a2ef13aa4e2436257acaf6d2a1b0e5a8e
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-06-03 08:41:35 -0400

    fix: add typed trigger variations

--- src/domain/schema/trigger.rs
+++ src/domain/schema/trigger.rs
@@ -8,14 +8,44 @@ use crate::models::iden::Iden;
 pub struct Trigger {
     pub name: String,
     pub table: Iden,
-    pub function: String,
+    pub function: Iden,
 
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub events: Vec<String>,
+    pub events: Vec<TriggerEvent>,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub timing: Option<String>,
+    pub timing: Option<TriggerTiming>,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub orientation: Option<String>,
+    pub orientation: Option<TriggerOrientation>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum TriggerEvent {
+    #[serde(rename = "INSERT")]
+    Insert,
+    #[serde(rename = "UPDATE")]
+    Update,
+    #[serde(rename = "DELETE")]
+    Delete,
+    #[serde(rename = "TRUNCATE")]
+    Truncate,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum TriggerTiming {
+    #[serde(rename = "BEFORE")]
+    Before,
+    #[serde(rename = "AFTER")]
+    After,
+    #[serde(rename = "INSTEAD OF")]
+    InsteadOf,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+pub enum TriggerOrientation {
+    #[serde(rename = "ROW")]
+    Row,
+    #[serde(rename = "STATEMENT")]
+    Statement,
 }
