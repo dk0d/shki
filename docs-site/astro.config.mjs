02eff5b423d9314054b85ff01e55a29b308f704e
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-08-13 14:46:43 -0400

    feat: initial docs site

---
+++ docs-site/astro.config.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import lucode from "lucode-starlight";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "My Docs",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/withastro/starlight",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: "Example Guide", slug: "guides/example" },
+					],
+				},
+				{
+					label: "Reference",
+					items: [{ autogenerate: { directory: "reference" } }],
+				},
+			],
+			plugins: [
+				lucode({
+					navLinks: [
+						{
+							label: "Hi There",
+							link: "#",
+						},
+					],
+				}),
+			],
+		}),
+	],
+});
